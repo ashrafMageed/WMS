209ac45e37fb4d54a27e07f239567e8d0296ed19
ashrafMageed <ashraftmageed@gmail.com>
2014-01-27 17:32:09 +0000

    Added walking skeleton

---
+++ src/WMS.UITests/Features/Products.feature
@@ -0,0 +1,15 @@
+﻿Feature: Products
+	In order to shop
+	As a customer
+	I want to be able to view products
+
+Background: 
+	Given I have the following products
+		|	Id	|	Name		|	Description				|	Price	|
+		|	1	|	PS4			|	New PS4 Console			|	350		| 
+		|	2	|	XBOX One	|	New Xbox One Console	|	430		|
+		|	3	|	Battlefield	|	Battlefield 4			|	50		|
+
+Scenario: View Products
+	When I navigate to the products page
+	Then I should see all products
---
+++ src/WMS.UITests/Steps/ProductsSteps.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+using WMS.DataStore;
+using WMS.Web.Controllers;
+using WMS.Web.Models;
+using WatiN.Core;
+using Table = TechTalk.SpecFlow.Table;
+using TableRow = TechTalk.SpecFlow.TableRow;
+
+namespace WMS.UITests.Steps
+{
+    [Binding]
+    public class ProductsSteps
+    {
+        private IE _ie;
+        private IList<Product> _givenProducts;
+
+        [Given(@"I have the following products")]
+        public void GivenIHaveTheFollowingProducts(Table tableOfProducts)
+        {
+            _givenProducts = tableOfProducts.Rows.Select(CreateProductFrom).ToList();
+
+            var db = Bootstrapper.Initialise();
+            db.Drop();
+            var respository = new Repository(db);
+            var controller = new ProductsController(respository);
+            _givenProducts.ToList().ForEach(controller.CreateProduct);
+        }
+
+        [When(@"I navigate to the products page")]
+        public void WhenINavigateToTheProductsPage()
+        {
+            _ie = new IE("http://localhost:62612/Products");
+        }
+        
+        [Then(@"I should see all products")]
+        public void ThenIShouldSeeAllProducts()
+        {
+            var products = _ie.Elements.Filter(Find.ByClass("product"));
+            products.Should().HaveCount(3);
+            products[0].InnerHtml.Should().Contain(_givenProducts[0].Name);
+            products[1].InnerHtml.Should().Contain(_givenProducts[1].Name);
+            products[2].InnerHtml.Should().Contain(_givenProducts[2].Name);
+        }
+
+        public static Product CreateProductFrom(TableRow productRow)
+        {
+            return new Product
+            {
+                Id = int.Parse(productRow["Id"]),
+                Name = productRow["Name"],
+                Description = productRow["Description"],
+                Price = decimal.Parse(productRow["Price"])
+            };
+        }
+    }
+}
+
